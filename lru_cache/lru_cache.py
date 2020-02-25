043d2102da532018d0182bdfa3716be51afb8526
Noah Franco <noahfranco@jonadabs-mini.lan>
2020-02-25 18:23:28 -0500

    completed get function

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -13,15 +13,16 @@ class LRUCache:
     to every node stored in the cache.
     """
 
+    # *** Plan ***
     # self.max = 0 ~ keeps track of the max number of nodes the DLL can hold
     # self.value = DoublyLinkedList() ~ list that holds the key-value entries in the correct order
     # self.storage = dict() ~ storage dict that provides fast access to every node stored in the cache
 
     def __init__(self, limit=10):
-        self.limit = 0
-        self.value = DoublyLinkedList()
-        self.storage = dict()
-
+        self.limit = limit
+        self.size = 0
+        self.list = DoublyLinkedList()
+        self.cache = dict()
 
 
     """
@@ -31,8 +32,20 @@ class LRUCache:
     Returns the value associated with the key or None if the
     key-value pair doesn't exist in the cache.
     """
+
+    # *** Plan ***
+    # need to find the key that's associated with the value so if key is in cache we are going to return
+    # once that's done we are going to move that node to the end of the list
+    # we are going to return the value and none if it is not in the catch
+
     def get(self, key):
-        pass
+        if key in self.cache: # if key is found in our cache
+            node = self.cache[key] # gives the value associated with the key 
+            self.list.move_to_end(node) # moves the key-value pair to the end of the list
+            return node.value[1] # the value associated with the key
+        else:
+            return None
+
 
     """
     Adds the given key-value pair to the cache. The newly-
@@ -44,5 +57,9 @@ class LRUCache:
     want to overwrite the old value associated with the key with
     the newly-specified value.
     """
+
+    # *** Plan ***
+    #
+
     def set(self, key, value):
         pass
